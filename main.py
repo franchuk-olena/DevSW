315e66a8f0b347331c7df47cc8110187bc263b32
Olena Franchuk <olenafranchuk@knu.ua>
2024-01-27 16:32:33 +0200

    Added vivaMechMat faculty!!!

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ def print_hello_10_times():
     for i in range(10):
         print("Hello, World")
 
-
 print_hello_10_times()
+
+
+def vivaMechMat():
+    print("Viva Mech-mat faculty!!!")
+
+vivaMechMat()
